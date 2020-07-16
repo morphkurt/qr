de2fe66e89643ab2a25906b04d710ac638778f54
d353163 <damitha.n.gunawardena@team.telstra.com>
2020-07-16 14:00:20 +1000

    adding qr app commit

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@ let count = 0;
 let qrcode
 let id
 let completed = false
-let ws = new WebSocket("https://qrdrop.herokuapp.com/");
+let ws = new WebSocket("wss://qrdrop.herokuapp.com/");
 
 
 $(document).ready(function () {
