3bfacd8046d1506bc1a64252003f75550c2ec74e
d353163 <damitha.n.gunawardena@team.telstra.com>
2020-07-16 13:56:47 +1000

    adding qr app commit

--- js/app.js
+++ js/app.js
@@ -2,10 +2,39 @@ let count = 0;
 let qrcode
 let id
 let completed = false
+let ws = new WebSocket("ws://localhost:3000");
+
 
 $(document).ready(function () {
+    id = makeid(25)
     qrcode = new QRCode(document.getElementById("qrcode"), '');
-    timeout();
+    let el;
+    ws.onmessage = (event) => {
+        let response = JSON.parse(event.data)
+        if (response.action == "id") {
+            qrcode.clear(); // clear the code.
+            qrcode.makeCode(response.id); // make another code.
+        }
+        console.log(response);
+        if (response.status == "success") {
+            qrcode.clear();
+            if (response.type == "image") {
+                let d = 'data:image/' + response.type + ';base64,' + response.content;
+                let w = window.open('about:blank');
+                let image = new Image();
+                image.src = d;
+                setTimeout(function () {
+                    w.document.write(image.outerHTML);
+                }, 0);
+            } else if (response.type == "url") {
+                window.location.href = atob(response.content);
+            }
+           
+        }
+    };
+  //  timeout(ws);
+
+
 
 })
 
@@ -19,41 +48,3 @@ function makeid(length) {
     return result;
 }
 
-function timeout() {
-    setTimeout(function () {
-
-        qrcode.clear(); // clear the code.
-        if ((count % 120) == 0) {
-            id = makeid(25)
-            qrcode.makeCode(id); // make another code.
-        }
-        if (((count % 3) == 0) && (!completed)) {
-            axios.get('https://whispering-waters-99783.herokuapp.com/qr/' + id,).then(({ data }) => {
-                if (data.status == "success") {
-                    completed = true;
-                    qrcode.clear();
-                    if (data.type) {
-                        let d = 'data:image/' + data.type + ';base64,' + data.content;
-                        let w = window.open('about:blank');
-                        let image = new Image();
-                        image.src = d;
-                        setTimeout(function () {
-                            w.document.write(image.outerHTML);
-                        }, 0);
-                    } else {
-                        window.location.href = atob(data.content);
-                    }
-                }
-            });
-        }
-
-
-
-        // Do Something Here
-        // Then recall the parent function to
-        // create a recursive loop.
-        $('#status').text("above code expires in " + (120 - (count % 120)) + " seconds")
-        count++
-        timeout();
-    }, 1000);
-}
